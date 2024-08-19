466fd6f3ef1c7f0f5ee7b303d8efdfe3cd6d60a4
Trikest <sal.00000850311@unicap.br>
2024-08-18 21:35:21 -0300

    No Ar

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
@@ -11,8 +15,9 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+    @GetMapping("/react1")
+    public ModelAndView index() throws ExecutionException, InterruptedException {
+         ModelAndView mav = new ModelAndView("react/index");
+        return mav;
     }
 }
